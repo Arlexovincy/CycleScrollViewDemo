152ec1548f9ffd53d593c9cba8cac0555ff70de5
Arlexovincy <546113971@qq.com>
2014-03-20 15:53:56 +0800

    Initial Commit

---
+++ CycleScrollViewDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CycleScrollViewDemo
+//
+//  Created by Foocaa on 14-3-20.
+//  Copyright (c) 2014年 Foocaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CycleScrollViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CycleScrollViewDemo
+//
+//  Created by Foocaa on 14-3-20.
+//  Copyright (c) 2014年 Foocaa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
